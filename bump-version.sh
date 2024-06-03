26ab9f9ba00c9732ccba77639e0d10a1f6362635
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-06-03 16:54:30 +0200

    bump to 0.2.0dev

--- bump-version.sh
+++ bump-version.sh
@@ -6,3 +6,4 @@ then
 fi
 sed -i -e "0,/<version>/{s/<version>.*<\\/version>/<version>$version<\\/version>/}" ./pom.xml
 sed -i -e "s/moduleVersion=.*/moduleVersion=$version/" ./src/main/resources/META-INF/nf-test-plugin
+./build.sh
